b4a31a45d01784677ba4f27da3706667963725bd
Uyen Giang Thai <153334214+gangisgiang@users.noreply.github.com>
2024-08-03 22:31:00 +1000

    Add files via upload

---
+++ 1.2/HelloWorld/Message.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace HelloWorld
+{
+    internal class Message
+    {
+        private string _text;
+
+        public Message(string text)
+        {
+            _text = text;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_text);
+        }
+
+    }
+}
