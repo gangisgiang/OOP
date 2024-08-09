9b659a99dd1677067584048eabbd991244beb8e8
Uyen Giang Thai <153334214+gangisgiang@users.noreply.github.com>
2024-08-09 21:26:25 +1000

    Add files via upload

---
+++ Week3/3.1/ClockClass/Clock.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace ClockClass
+{
+	public class Clock
+	{
+		private Counter _hour;
+		private Counter _minute;
+		private Counter _second;
+
+		public Clock()
+		{
+			_hour = new Counter(12);
+			_minute = new Counter(60);
+			_second = new Counter(60);
+		}
+
+		public void Tick()
+		{
+			_second.Increment();
+			if (_second.Tick() == 0)
+			{
+				_minute.Increment();
+				if (_minute.Tick() == 0)
+				{
+					_hour.Increment();
+				}
+			}
+		}
+
+		public void Reset()
+		{
+			_hour.Reset();
+			_minute.Reset();
+			_second.Reset();
+		}
+
+		public void Set(int hour, int minute, int second)
+		{
+			_hour.Set(hour);
+			_minute.Set(minute);
+			_second.Set(second);
+		}
+
+		public string Display()
+		{
+			string hour = _hour.Tick().ToString("D2");   // Ensures two digits
+			string minute = _minute.Tick().ToString("D2");
+			string second = _second.Tick().ToString("D2");
+
+			return $"{hour}:{minute}:{second}";
+		}
+	}
+}
+
---
+++ Week3/3.1/ClockClass/Counter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace ClockClass
+{
+	public class Counter
+	{
+        private int _count;
+        private int _rollover;
+
+        public Counter(int rollover)
+        {
+            _rollover = rollover;
+            _count = 0;
+        }
+
+        public void Increment()
+        {
+            _count++;
+            if (_count >= _rollover)
+            {
+                _count = 0;
+            }
+        }
+
+        public void Reset()
+        {
+            _count = 0;
+        }
+
+        public void Set(int value)
+        {
+            if (value >= 0 && value < _rollover)
+            {
+                _count = value;
+            }
+        }
+
+        public int Tick()
+        {
+            return _count;
+        }
+    }
+}
