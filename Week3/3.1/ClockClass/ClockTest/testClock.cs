8db1caeadbc9ac698101299c16ea4b5fddde1fe5
Uyen Giang Thai <153334214+gangisgiang@users.noreply.github.com>
2024-08-09 21:21:40 +1000

    Add files via upload

---
+++ Week3/3.1/ClockClass/ClockTest/testClock.cs
@@ -0,0 +1,45 @@
+﻿using ClockClass;
+
+namespace ClockTest;
+
+public class ClockTest
+{
+    private Clock _clock;
+    [SetUp]
+    public void Setup()
+    {
+        _clock = new Clock();
+    }
+
+    [Test]
+    public void TickTest()
+    {
+        _clock.Set(0, 0, 0);
+        _clock.Tick();
+        Assert.AreEqual("00:00:01", _clock.Display());
+    }
+
+    [Test]
+    public void ResetTest()
+    {
+        _clock.Set(0, 0, 0);
+        _clock.Tick();
+        _clock.Reset();
+        Assert.AreEqual("00:00:00", _clock.Display());
+    }
+
+    [Test]
+    public void SetTest()
+    {
+        _clock.Set(1, 2, 3);
+        Assert.AreEqual("01:02:03", _clock.Display());
+    }
+
+    [Test]
+    public void RolloverTest()
+    {
+        _clock.Set(11, 59, 59);
+        _clock.Tick();
+        Assert.AreEqual("00:00:00", _clock.Display());
+    }
+}
