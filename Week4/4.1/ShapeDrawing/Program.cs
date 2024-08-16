5cc13312a2e7b5d62562950d2580d0ffeac11034
gangisgiang <tuyengiang284@gmail.com>
2024-08-17 00:05:09 +1000

    f

--- Week4/4.1/ShapeDrawing/Program.cs
+++ Week4/4.1/ShapeDrawing/Program.cs
@@ -7,6 +7,45 @@ namespace ShapeDrawing
     {
         public static void Main()
         {
+            Window window = new Window("Shape Drawing", 800, 600);
+            Drawing myDrawing = new Drawing();
+            do
+            {
+                SplashKit.ProcessEvents();
+                SplashKit.ClearScreen();
+
+                if (SplashKit.MouseClicked(MouseButton.LeftButton))
+                {
+                    Shape newShape = new Shape(110);
+                    newShape.X = SplashKit.MouseX();
+                    newShape.Y = SplashKit.MouseY();
+                    myDrawing.AddShape(newShape);
+                }
+
+                if (SplashKit.MouseClicked(MouseButton.RightButton))
+                {
+                    Point2D pt = SplashKit.MousePosition();
+                    myDrawing.SelectShapesAt(pt);
+                }
+
+                if (SplashKit.KeyTyped(KeyCode.SpaceKey))
+                {
+                    myDrawing.Background = SplashKit.RandomRGBColor(255);
+                }
+
+                if (SplashKit.KeyTyped(KeyCode.DeleteKey) || SplashKit.KeyTyped(KeyCode.BackspaceKey))
+                {
+                    foreach (Shape s in myDrawing.SelectedShapes)
+                    {
+                        myDrawing.RemoveShape(s);
+                    }
+                }
+
+                myDrawing.Draw();
+                SplashKit.RefreshScreen();
+            }
+
+            while (!window.CloseRequested);
 
         }
     }
---
+++ Week4/4.1/ShapeDrawing/Shape.cs
@@ -0,0 +1,79 @@
+using SplashKitSDK;
+
+namespace ShapeDrawing
+{
+    public class Shape
+    {
+        private Color _color;
+        private float _x, _y;
+        private int _width, _height;
+        private bool _selected;
+
+        public Shape(int param)
+        {
+            _color = Color.Chocolate;
+            _x = 0.0f;
+            _y = 0.0f;
+            _width = param;
+            _height = param;
+        }
+
+        public bool Selected
+        {
+            get { return _selected; }
+            set { _selected = value; }
+        }
+
+        public void Draw()
+        {
+            if (_selected)
+            {
+                DrawOutline();
+            }
+            SplashKit.FillRectangle(_color, _x, _y, _width, _height);
+        }
+
+        public bool IsAt(Point2D pt)
+        {
+            return (pt.X >= _x && pt.X <= _x + _width)
+                && (pt.Y >= _y && pt.Y <= _y + _height);
+        }
+
+        public Color Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public float X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public float Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public void DrawOutline()
+        {
+            int value = 5;
+            SplashKit.FillRectangle(Color.Black, _x - value, _y - value,
+                                    _width + value * 2, _height + value * 2);
+        }
+    }
+}
