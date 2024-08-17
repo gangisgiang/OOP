f870a660eb85db64ec38a9db2eb0240c3ca9c5c9
gangisgiang <tuyengiang284@gmail.com>
2024-08-18 05:39:24 +1000

    thich the

---
+++ Week4/4.2/SwinAdventure/InventoryTests/testInventory.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using SwinAdventure;
+
+namespace InventoryTests;
+
+public class InventoryTest
+{
+
+    private Inventory _inventory;
+    private Item _item;
+
+    [SetUp]
+    public void Setup()
+    {
+        _inventory = new Inventory();
+        _item = new Item(new string[] { "id1", "id2" }, "name", "desc");
+    }
+
+    [Test]
+    public void FindItem()
+    {
+        _inventory.Put(_item);
+        Assert.AreEqual(_item, _inventory.Fetch("id1"));
+        Assert.AreEqual(_item, _inventory.Fetch("id2"));
+    }
+
+    [Test]
+    public void NoItemFound()
+    {
+        Assert.AreEqual(null, _inventory.Fetch("id1"));
+    }
+
+    [Test]
+    public void FetchItem()
+    {
+        _inventory.Put(_item);
+        Assert.AreEqual(_item, _inventory.Fetch("id1"));
+        Assert.AreEqual(_item, _inventory.Fetch("id2"));
+        Assert.AreEqual(null, _inventory.Fetch("id3"));
+    }
+
+    [Test]
+    public void TakeItem()
+    {
+        _inventory.Put(_item);
+        Assert.AreEqual(_item, _inventory.Take("id1"));
+        Assert.AreEqual(null, _inventory.Fetch("id1"));
+    }
+
+    [Test]
+    public void ItemList()
+    {
+        _inventory.Put(_item);
+        Assert.AreEqual("name (id1) - name (id2)", _inventory.ItemList);
+    }
+}
---
+++ Week4/4.2/SwinAdventure/PlayerTests/testPlayer.cs
@@ -0,0 +1,31 @@
+﻿using SwinAdventure;
+
+namespace PlayerTests;
+
+public class PlayerTest
+{
+    private Player _player;
+    private Item _item;
+    [SetUp]
+    public void Setup()
+    {
+        _player = new Player("name", "desc");
+        Item _item = new Item(new string[] { "id1", "id2" }, "name", "desc");
+
+    }
+
+    [Test]
+    public void FindItem()
+    {
+        _player.Inventory.Put(item);
+        Assert.AreEqual(_item, _player.Locate("id1"));
+        Assert.AreEqual(_item, _player.Locate("id2"));
+        Assert.AreEqual(null, _player.Locate("id3"));
+    }
+
+    [Test]
+    public void NoItemFound()
+    {
+        Assert.AreEqual(null, _player.Locate("id1"));
+    }
+}
