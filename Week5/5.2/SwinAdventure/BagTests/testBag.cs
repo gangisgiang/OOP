2386d3c8aa40386974512299ce74c28bd3c4d09a
gangisgiang <tuyengiang284@gmail.com>
2024-08-23 22:22:33 +1000

    Update testBag.cs

--- Week5/5.2/SwinAdventure/BagTests/testBag.cs
+++ Week5/5.2/SwinAdventure/BagTests/testBag.cs
@@ -4,14 +4,41 @@ using SwinAdventure;
 
 public class Tests
 {
+    private Bag _bag;
+    private Item _item;
+
     [SetUp]
     public void Setup()
     {
+        _bag = new Bag(new string[] { "id1", "id2" }, "name", "desc");
+        _item = new Item(new string[] { "id1", "id2" }, "name", "desc");
     }
 
     [Test]
-    public void Test1()
+    public void LocateItems()
     {
-        Assert.Pass();
+        _bag.Inventory.Put(_item);
+        Assert.AreEqual(_item, _bag.Locate("id1"));
+        Assert.AreEqual(_item, _bag.Locate("id2"));
+    }
+
+    [Test]
+    public void LocateItself()
+    {
+        Assert.AreEqual(_bag, _bag.Locate("id1"));
+        Assert.AreEqual(_bag, _bag.Locate("id2"));
+    }
+
+    [Test]
+    public void LocateNothing()
+    {
+        Assert.IsNull(_bag.Locate("Giang"));
+    }
+
+    [Test]
+    public void FullDescription()
+    {
+        _bag.Inventory.Put(_item);
+        Assert.AreEqual("In the name you can see:\n\ta name (id1)\n\tdesc\n", _bag.FullDescription);
     }
 }
