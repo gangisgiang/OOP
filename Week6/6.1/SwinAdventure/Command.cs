64e49f933eb3f46c14f74f9cd6f8f6556d255b50
gangisgiang <tuyengiang284@gmail.com>
2024-08-31 04:12:29 +1000

    bede

---
+++ Week6/6.1/SwinAdventure/Command.cs
@@ -0,0 +1,13 @@
+using System;
+namespace SwinAdventure
+{
+	public abstract class Command : IdentifiableObject
+	{
+		public Command(string[] ids) : base(ids)
+        {
+        }
+
+        public abstract string Execute(Player p, string[] text);
+	}
+}
+
