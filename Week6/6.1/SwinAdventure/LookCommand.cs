ce6464222f7c007eb30ed3b8daad76dbf4ee1d3d
gangisgiang <tuyengiang284@gmail.com>
2024-08-31 23:51:04 +1000

    Update LookCommand.cs

--- Week6/6.1/SwinAdventure/LookCommand.cs
+++ Week6/6.1/SwinAdventure/LookCommand.cs
@@ -3,42 +3,47 @@ namespace SwinAdventure
 {
 	public class LookCommand : Command
 	{
-		public LookCommand()
-		{
-            Identifiers = new string[] { "look" };
-		}
+		public LookCommand() : base(new string[] { "look"})
+        {
+        }
 
         public override string Execute(Player p, string[] text)
         {
-            if (text.Length != 3 || text.Length != 5)
+            if (text.Length != 3 && text.Length != 5)
             {
-                return "I don't know how to look like that";
+                return "I don’t know how to look like that";
             }
+
             if (text[0] != "look")
             {
                 return "Error in look input";
             }
+
             if (text[1] != "at")
             {
                 return "What do you want to look at?";
             }
 
-            if (text.Length == 5)
+            if (text.Length == 5 && text[3] != "in")
             {
-                if (text[3] != "in")
-                {
-                    return "What do you want to look in?";
-                }
-                IHaveInventory container = p.Locate(text[4]) as IHaveInventory;
+                return "What do you want to look in?";
+            }
+
+            IHaveInventory container = null;
+            if (text.Length == 3)
+            {
+                container = p;
+            }
+            else
+            {
+                container = FetchContainer(p, text[4]);
                 if (container == null)
                 {
                     return "I cannot find the " + text[4];
                 }
-                return container.Locate(text[2]).FullDescription;
             }
-            return p.Locate(text[2]).FullDescription;
 
-            
+            return LookAtIn(text[2], container);
         }
 
 
