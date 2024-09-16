6c5cebec727a00a6eabafeb4a7bd577171ea02a4
gangisgiang <tuyengiang284@gmail.com>
2024-09-17 02:31:59 +1000

    bede

---
+++ Week7/6.1/SwinAdventure/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace SwinAdventure
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Please describe yourself: ");
+            string desc = Console.ReadLine();
+
+            Player player = new Player(name, desc);
+
+            Console.WriteLine("You are " + player.Name + " " + player.Description);
+
+            Item item1 = new Item(new string[] { "sword" }, "sword", "a sharp sword");
+            player.Inventory.Put(item1);
+            item item2 = new Item(new string[] { "shield" }, "shield", "a strong shield");
+            player.Inventory.Put(item2);
+
+            Bag bag = new Bag(new string[] { "bag" }, "bag", "a green bag");
+            player.Inventory.Put(bag);
+
+            Item item3 = new Item(new string[] { "coin" }, "coin", "a shiny coin");
+            bag.Inventory.Put(item3);
+
+            // Loop reading commands from the user, and getting the look command to execute them
+
+            LookCommand look = new LookCommand();
+            string command = "";
+            while (command != "quit")
+            {
+                Console.WriteLine("Enter a command: ");
+                command = Console.ReadLine();
+                string[] parts = command.Split(' ');
+                string result = look.Execute(player, parts);
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
--- Week7/6.1/SwinAdventure/obj/Debug/net7.0/SwinAdventure.AssemblyInfo.cs
+++ Week7/6.1/SwinAdventure/obj/Debug/net7.0/SwinAdventure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwinAdventure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c6df94d5083bdf69bf313403fc71d1f533d0014")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwinAdventure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwinAdventure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
